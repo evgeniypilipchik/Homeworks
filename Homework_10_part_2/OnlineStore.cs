f51f0c6f15765ee829b72de924c1141385ce8665
evgeniypilipchik <evgeniypilipchik@yandex.ru>
2025-04-05 15:12:15 +0300

    add Homework_10_part_2

---
+++ Homework_10_part_2/OnlineStore.cs
@@ -0,0 +1,24 @@
+﻿namespace Homework_10_part_2
+{
+    internal class OnlineStore
+    {
+
+        public string OrderName;
+        public int OrderId;
+
+        public OnlineStore(string name, int id)
+        {
+            OrderName = name;
+            OrderId = id;
+        }
+
+        public delegate void OrderHandler(int orderId);
+        public event OrderHandler? OnOrderProcessed;
+
+        public void ProcessOrder(int orderId)
+        {
+            Console.WriteLine($"Ваш заказ {orderId}\n");
+            OnOrderProcessed?.Invoke(orderId);
+        }
+    }
+}
---
+++ Homework_10_part_2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Homework_10_part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<OnlineStore> products = new List<OnlineStore>()
+            {
+                new OnlineStore("Конфетница", 1001),
+                new OnlineStore("Зеркало", 1002),
+                new OnlineStore("Сервиз", 1003),
+                new OnlineStore("Ваза", 1004),
+            };
+
+            Console.WriteLine("Выберите товар из списка:");
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {products[i].OrderName}");
+                products[i].OnOrderProcessed += NotifyClient;
+                products[i].OnOrderProcessed += NotifyManager;
+            }
+
+            Console.WriteLine();
+            int productType = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            products[productType - 1].ProcessOrder(products[productType - 1].OrderId);
+        }
+
+        static void NotifyClient(int orderId) => Console.WriteLine($"Ваш заказ {orderId} готов!");
+
+        static void NotifyManager(int orderId) => Console.WriteLine($"Заказ {orderId} обработан.");
+    }
+}
