fda6d9eecf190e3d99045e03e0a3e27130fa8735
evgeniypilipchik <evgeniypilipchik@yandex.ru>
2025-04-06 19:42:27 +0300

    add Homework_11

---
+++ Homework_11/ComparablePair.cs
@@ -0,0 +1,34 @@
+﻿namespace Homework_11
+{
+    internal class ComparablePair<T1, T2> where T1 : IComparable<T1> where T2 : IComparable<T2>
+    {
+        public T1 T1Value { get; }
+
+        public T2 T2Value { get; }
+
+        public ComparablePair(T1 T1Value, T2 T2Value)
+        {
+            this.T1Value = T1Value;
+            this.T2Value = T2Value;
+        }
+
+        public static ComparablePair<T1, T2> Max(ComparablePair<T1, T2> pair1, ComparablePair<T1, T2> pair2)
+        {
+            return pair1.CompareTo(pair2) > 0 ? pair1 : pair2;
+        }
+
+        public int CompareTo(ComparablePair<T1, T2> other)
+        {
+            var firstComparision = T1Value.CompareTo(other.T1Value);
+
+            if (firstComparision != 0)
+            {
+                return firstComparision;
+            }
+            else
+            {
+                return T2Value.CompareTo(other.T2Value);
+            }
+        }
+    }
+}
---
+++ Homework_11/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new Pair<int, string>(123, "Hello");
+            Console.WriteLine(String.Join(", ", items.T1Item, items.T2Item));
+
+            var values1 = new ComparablePair<int, string>(456, "Hello");
+            var values2 = new ComparablePair<int, string>(456, "Helloo");
+            var maxValue =  ComparablePair<int, string>.Max(values1, values2);
+            Console.WriteLine(String.Join(", ", maxValue.T1Value, maxValue.T2Value));
+        }
+    }
+}
