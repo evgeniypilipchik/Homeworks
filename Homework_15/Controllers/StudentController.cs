be0a4591e9a3454d7caf45c84df50ce0060aaa56
evgeniypilipchik <evgeniypilipchik@yandex.ru>
2025-04-26 16:56:26 +0300

    add Homework_15

---
+++ Homework_15/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using Homework_15.Models;
+using Homework_15.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace Homework_15.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var students = StudentRepository.GetStudents();
+            return View(students);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            StudentRepository.Add(student);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            StudentRepository.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var student = StudentRepository.Details(id);
+            return View(student);
+        }
+    }
+}
---
+++ Homework_15/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace Homework_15.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool Status { get; set; }
+        public double Grade { get; set; }
+        public string Gender { get; set; }
+        public string Temperament { get; set; }
+    }
+}
